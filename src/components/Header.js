79df3af9454185da3f2984f43aa51fed85d26988
Owner <owner@owners-mbp.attlocal.net>
2021-09-23 16:39:52 -0400

    created header component Cleverette

---
+++ src/components/Header.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Component } from "react";
+
+
+class Header extends Component {
+    constructor() {
+        super()
+    }
+    render() {
+        return(
+            <div>
+                <h1>Header</h1>
+            </div>
+        )
+    }
+}
+
+export default Header;
